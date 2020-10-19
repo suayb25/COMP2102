ee1ca2615856612a58178692cd3d0cc926d2cdf3
suayb25 <suayb.ozcelik@isik.edu.tr>
2020-10-19 19:22:14 +0300

    Linked List removeLast updates

--- src/LinkedList/CSE202_Lab_2.java
+++ src/LinkedList/CSE202_Lab_2.java
@@ -4,23 +4,29 @@ public class CSE202_Lab_2 {
     public static void main(String[] args) {
         MyLinkedList ml = new MyLinkedList();
         ml.insertSorted(100);
+        System.out.println(ml.toString());
         ml.insertSorted(112);
-
+        System.out.println(ml.toString());
         ml.removeLast();
+        System.out.println(ml.toString());
         ml.insertSorted(202);
+        ml.insertSorted(302);
         System.out.println(ml.toString());
         ml.removeFirst();
         System.out.println(ml.toString());
-        ml.insertSorted(150);
+        ml.insertSorted(750);
         System.out.println(ml.toString());
         ml.removeLast();
         System.out.println(ml.toString());
-        System.out.println(ml.search(650));
+        System.out.println(ml.search(302));
         System.out.println("sum= " + ml.getSum());
-
+        ml.removeLast();
+        ml.removeLast();
+        ml.insertFirst(100);
+        ml.insertFirst(300);
         ml.insertAfter(ml.first, 500);
         System.out.println(ml.toString());
-        System.out.println(ml.search(500));
+        System.out.println(ml.search(1000));
         ml.insertSorted(600);
         ml.insertSorted(25);
         System.out.println(ml.toString());
@@ -28,7 +34,7 @@ public class CSE202_Lab_2 {
 
         /*ml.insertAfterV2(112,272);
         System.out.println(ml.toString());*/
-
+        System.out.println("-------------------");
         MyLinkedList[] a = new MyLinkedList[2];
         a[1] = ml;
         a[0] = ml;
