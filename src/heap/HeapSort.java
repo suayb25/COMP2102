7169f5816f6c0c4b267da5a0b92b1a1aa50de97a
suayb25 <suayb.ozcelik@isik.edu.tr>
2020-12-14 16:33:52 +0300

    HeapSort

--- src/heap/HeapSort.java
+++ src/heap/HeapSort.java
@@ -1,52 +1,74 @@
 package heap;
 
+import java.util.Random;
+
 public class HeapSort {
+
+    private static int elementCompare = 0;
+    private static int elementSwaps = 0;
 
     public static void sort(int[] arIn) {
         int N = arIn.length;
         //heapify
         for (int k = N / 2; k >= 1; k--) {
-            sink(arIn,k,N);
+            sink(arIn, k, N);
         }
         System.out.println("Heapified: ");
         print(arIn);
-        for(int k = arIn.length; k>1;){
-            swap(arIn,1,k--);
-            sink(arIn,1,k);
+        for (int k = arIn.length; k > 1; ) {
+            swap(arIn, 1, k--);
+            sink(arIn, 1, k);
         }
     }
 
     public static void sink(int[] ar, int k, int h) {
         while ((2 * k) <= h) {
             int mx = 2 * k;
-            if ( mx < h && ar[mx - 1] < ar[mx]) {
+            elementCompare++;
+            if (mx < h && ar[mx - 1] < ar[mx]) {
                 mx++;
             }
-            if (ar[k - 1] >= ar[mx - 1]){
+            elementCompare++;
+            if (ar[k - 1] >= ar[mx - 1]) {
                 break;
             }
-            swap(ar,k,mx);
-            k=mx;
+            swap(ar, k, mx);
+            k = mx;
         }
     }
 
-    public static void swap(int[] ar,int i, int j) { //exch
-        int tmp = ar[i-1];
-        ar[i-1] = ar[j-1];
-        ar[j-1] = tmp;
+    public static void swap(int[] ar, int i, int j) { //exch
+        elementSwaps++;
+        int tmp = ar[i - 1];
+        ar[i - 1] = ar[j - 1];
+        ar[j - 1] = tmp;
     }
 
-   public static void print(int[] ar){
-       for(int i=0;i<ar.length;i++){
-           System.out.print(ar[i]+" ");
-       }
-       System.out.println();
-   }
+    public static void print(int[] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(ar[i] + " ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
-        int[] a = {2,5,60,50,11,12,6,7,23,9,10,35};
+        Random rnd = new Random();
+        int[] randomArray = new int[1024];
+        for (int i = 0; i < randomArray.length; i++) {
+            randomArray[i] = rnd.nextInt(1024);
+        }
+        print(randomArray);
+        sort(randomArray);
+        print(randomArray);
+        System.out.println("Element Compare Count = " + elementCompare);
+        System.out.println("Element Swap Count = " + elementSwaps);
+
+        System.out.println("---------");
+        int[] a = {2, 5, 60, 50, 11, 12, 6, 7, 23, 9, 10, 35};
         print(a);
         sort(a);
         print(a);
+        System.out.println("Element Compare Count = " + elementCompare);
+        System.out.println("Element Swap Count = " + elementSwaps);
     }
 }
